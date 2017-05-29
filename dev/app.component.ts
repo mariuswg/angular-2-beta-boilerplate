f0b1af7ab49482ff1b93e407d5d806ae75fa7eeb
Marius Gynnild <mwgynnild@gmail.com>
2017-05-29 21:39:46 +0200

    Add basic actions to be performed by the user

--- dev/app.component.ts
+++ dev/app.component.ts
@@ -4,9 +4,21 @@ import {Component} from 'angular2/core';
     selector: 'my-app',
     template: `
         <h1>Angular 2 Boilerplate</h1>
-        <p>Hello World!</p>
+        <p>Hello {{contact.firstName}}</p>
+        <p (click)="onSelect()" [class.clicked]="showDetail === true">Reveal contact information by clicking on this text</p>
+        <div *ngIf="showDetail === true">
+            Phone: {{contact.phone}} <br>
+            Email: {{contact.email}}
+        </div>
+        <h4>Change my name in the input field below</h4>
+        <input [(ngModel)]="contact.firstName" type="text">
     `,
 })
 export class AppComponent {
+    public contact = { firstName: "Marius", phone:"12345678", email:"e@mail.com"};
+    public showDetail = false;
 
+    onSelect(){
+      this.showDetail = true;
+    }
 }
